c0efce28f6a214e38666c51dca3cbe9c3ada3358
unknown <you@example.com22521660@gm.uit.edu.vn>
2025-05-03 12:34:41 +0700

    right sidebar + home

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -18,18 +18,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/private/Home/Home";
 import ChannelSidebar from "./pages/private/Home/LeftSideBar";
+import RightSidebarGrChat from "./pages/private/Home/RightSidebarGrChat";
 import SideBar from "./pages/private/Home/SideBar";
+import TopBarChannel from "./pages/private/Home/TopBarGrChat";
+import WelcomePage from "./pages/private/Home/WelcomePage";
 
 function AppLayout() {
   return (
-    <div className="flex">
+    <div className="flex h-screen overflow-hidden">
       <div className="w-[6%] bg-[#18092f]">
         <SideBar />
       </div>
-      <div className="w-[18%]">
+
+      <div className="w-[18%] bg-[#2b2d31]">
         <ChannelSidebar />
       </div>
-      <div className="flex-2 bg-gray-100"> {/* Nội dung chính */}</div>
+
+      <div className="flex-1 relative bg-gray-100">
+        <TopBarChannel />
+        <div className="pt-[60px] h-full overflow-y-auto flex justify-center items-center">
+          <WelcomePage /> {/* 👈 thêm dòng này */}
+        </div>{" "}
+      </div>
+
+      <RightSidebarGrChat />
     </div>
   );
 }
